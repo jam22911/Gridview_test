9b1d32f6900ddc0bd8e504446dc774c66f4d8861
jam22911 <43859534+jam22911@users.noreply.github.com>
2018-10-10 16:49:35 +0700

    Add files via upload

---
+++ Listdb.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Listdb.aspx.cs" Inherits="Web.Listdb" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="gvdb" runat="server" AutoGenerateColumns="true">
+    </asp:GridView>
+</asp:Content>
---
+++ Listdb.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Web.Repositories;
+
+namespace Web
+{
+    public partial class Listdb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                bindData();
+            }
+        }
+
+        void bindData()
+        {
+            Repository movieRepo = new Repository();
+            gvdb.DataSource = movieRepo.getList();
+            gvdb.DataBind();
+        }
+    }
+}
